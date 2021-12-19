e55159e630cc6908009c712a8119a24c6d974c59
HighTechLowIQ <hightechlowiq@gmail.com>
2021-12-19 13:21:35 +0000

    Updating member counter to use member.presence.status, and calling it from the ready.js

--- modules/member-counter.js
+++ modules/member-counter.js
@@ -1,42 +1,19 @@
 require('dotenv').config();
 
 module.exports = async (client) => {
-    const guild = client.guilds.cache.get(process.env.SERVER_ID);
+    const guild = client.guilds.cache.get(process.env.GUILD_ID);
 
     setInterval(() => {
         guild.members.fetch().then(async fetchedMembers => {
-            // TODO : Is there a reason we can't use totalOnline?
-            // let totalOnline = fetchedMembers.filter(member => member.presence.status !== 'offline').size;
-
-            let totalIdle = fetchedMembers.filter(member => member.presence.status === 'idle').size;
-            let totalDnd = fetchedMembers.filter(member => member.presence.status === 'dnd').size;
-
-            let activityTypes = ["STREAMING", "PLAYING", "WATCHING", "COMPETING", "CUSTOM"];
-            let totalMap = {};
-
-            for (let j = 0; j < activityTypes.length; j++) {
-                let activityType = activityTypes[j];
-                // We init the totalMap with a 0 for each activity straight away, so it's not undefined when we fetch it later
-                totalMap.set(activityType, 0);
-                for (let i = 0; i < 5; i++) {
-                    fetchedMembers.forEach(member => {
-                        if (member.presence.activities[i] && member.presence.activities[i].type === activityType) {
-                            let amount = totalMap.get(activityType) + 1;
-                            totalMap.set(activityType, amount);
-                        }
-                    })
-                }
-            }
-
-            let totalMapSum = Object.values(totalMap).reduce((accumulator, value) => accumulator + value, 0);
-            let totalAll = totalIdle + totalDnd + totalMapSum + 130;
+            // TODO : Check why discord is reporting the user number incorrectly - off by roughly 130
+            let totalOnline = fetchedMembers.filter(member => member.presence.status !== 'offline').size + 130;
             let memberCount = guild.memberCount;
 
             function kFormatter(num) {
-                return Math.abs(num) > 999 ? Math.sign(num) * ((Math.abs(num) / 1000).toFixed(2)) + 'K' : Math.sign(num) * Math.abs(num)
+                return Math.abs(num) > 999 ? Math.sign(num) * ((Math.abs(num) / 1000).toFixed(2)) + 'K' : Math.sign(num) * Math.abs(num);
             }
 
-            let onlineReal = kFormatter(totalAll);
+            let onlineReal = kFormatter(totalOnline);
             let totalReal = kFormatter(memberCount);
 
             const channelOnline = guild.channels.cache.get(process.env.VC_ONLINE);
