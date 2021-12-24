d1b9ce183e8bbddf635f6c3282dddf058dc7fdcc
ProbablyRaging <probablyraging@gmail.com>
2021-12-25 01:52:41 +1100

    Rank and level auto-role features

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -2,6 +2,7 @@ const { Message } = require('discord.js');
 const linkCooldown = require('../../modules/link_cooldown');
 const ckqPost = require('../../modules/ckq_post');
 const bumpPost = require('../../modules/bump_post');
+const levelsCheck = require('../../modules/levels_check');
 
 module.exports = {
     name: `messageCreate`,
@@ -14,5 +15,13 @@ module.exports = {
         linkCooldown(message, client, Discord);
         ckqPost(message, client, Discord);
         bumpPost(message, client, Discord);
+        levelsCheck(message, client, Discord);
+
+        // delete posts containing tweets in the insider channel
+        if (message?.channel.id === process.env.INSIDER_CHAN) {
+            if (message?.content.toLowerCase().includes('tweet')) {
+                message?.delete().catch(err => { return; });
+            }
+        }
     }
 }
