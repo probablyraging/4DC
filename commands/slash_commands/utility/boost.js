95c3a11fcfaf212414a3e848fc3cc3d53488622d
ProbablyRaging <probablyraging@gmail.com>
2021-12-29 23:57:51 +1100

    Added boost command

---
+++ commands/slash_commands/utility/boost.js
@@ -0,0 +1,25 @@
+const { ContextMenuInteraction } = require('discord.js');
+const path = require('path');
+
+module.exports = {
+    name: `boost`,
+    description: `Provides information about the current server booster perks`,
+    permission: ``,
+    cooldown: 5,
+    type: `CHAT_INPUT`,
+    usage: `/boost`,
+    /**
+     * 
+     * @param {ContextMenuInteraction} interaction 
+     */
+    execute(interaction) {
+        const { } = interaction;
+
+        const image = 'https://www.weebly.com/editor/uploads/1/2/6/0/126006118/custom_themes/656977109613806662/files/images/CHBoost.png';
+
+        interaction.reply({
+            files: [image],
+            ephemeral: true
+        }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending an interaction: `, err));
+    }
+}
