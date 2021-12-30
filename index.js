5866fe46731ea77748cc75e573d154777a5ee0c9
ProbablyRaging <probablyraging@gmail.com>
2021-12-31 03:26:49 +1100

    Testing pm2-auto-pull

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ client.setMaxListeners(0);
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
+// test
+
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
 })
