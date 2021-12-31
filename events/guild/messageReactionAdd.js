2e781d5fac4d52ee764e46e2929fbe5ae5681e8b
ProbablyRaging <probablyraging@gmail.com>
2022-01-01 06:37:21 +1100

    Fuck github, honestly

---
+++ events/guild/messageReactionAdd.js
@@ -0,0 +1,51 @@
+const selfRoleReactions = require("../../objects/selfRoleReactions");
+
+module.exports = {
+    name: "messageReactionAdd",
+    async execute(reaction, user, client) {
+        // We don't care if a bot reacts
+        if (user.bot) {
+            return;
+        }
+
+        if (reaction.partial) {
+            try {
+                await reaction.fetch();
+            } catch (error) {
+                console.error("Could not fetch the message from the reaction:", error);
+                return;
+            }
+        }
+
+        let message = reaction.message;
+        // We only listen for reactions in the self-roles channel
+        if (process.env.SELFROLE_CHAN === message.channel.id) {
+            let emoji = reaction.emoji;
+            const guild = client.guilds.cache.get(process.env.GUILD_ID);
+            const member = guild.members.cache.find(member => member.id === user.id);
+            if (member) {
+                for (let reactionKey in selfRoleReactions) {
+                    if (selfRoleReactions.hasOwnProperty(reactionKey)) {
+                        let selfRoleMessage = selfRoleReactions[reactionKey];
+                        for (let reactionClass in selfRoleMessage) {
+                            let selfRoleClass = selfRoleMessage[reactionClass];
+                            // Check if we have the right message from self-roles
+                            if (selfRoleClass.messageId === message.id) {
+                                for (let roleIdKey in selfRoleClass.roleIds) {
+                                        let roleId = selfRoleClass.roleIds[roleIdKey];
+                                        if (roleIdKey === emoji.name) {
+                                            member?.roles.add(roleId);
+                                        } else if (selfRoleClass.exclusive) {
+                                            message.reactions.resolve(roleIdKey).users.remove(member?.id);
+                                            member?.roles.remove(roleId);
+                                        }
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
---
+++ objects/selfRoleReactions.js
@@ -0,0 +1,68 @@
+const emojis = require("../lists/role-emojis");
+
+const selfRoleReactions = {
+    "Platforms": {
+        "messageId": process.env.SELF_ROLES_PLATFORM_ID,
+        "exclusive": false,
+        "roleIds": {
+            "twitch": emojis.twitch,
+            "youtube": emojis.youtube,
+            "instagram": emojis.instagram,
+            "tiktok": emojis.tiktok
+        }
+    },
+    "NicknameColor": {
+        "messageId": process.env.SELF_ROLES_NICKNAME_COLOR_ID,
+        "exclusive": true,
+        "roleIds": {
+            "🔵": emojis.a,
+            "🔴": emojis.b,
+            "🟢": emojis.c,
+            "🟠": emojis.d,
+            "🟡": emojis.e,
+            "🌸": emojis.f,
+            "🟣": emojis.g
+        }
+    },
+    "Age": {
+        "messageId": process.env.SELF_ROLES_AGE_ID,
+        "exclusive": true,
+        "roleIds": {
+            "👶": emojis.h,
+            "👦": emojis.i,
+            "👨": emojis.j
+        }
+    },
+    "Region": {
+        "messageId": process.env.SELF_ROLES_REGION_ID,
+        "exclusive": true,
+        "roleIds": {
+            "1️⃣": emojis.k,
+            "2️⃣": emojis.l,
+            "3️⃣": emojis.m,
+            "4️⃣": emojis.n
+        }
+    },
+    "Gender": {
+        "messageId": process.env.SELF_ROLES_GENDER_ID,
+        "exclusive": true,
+        "roleIds": {
+            "🙋‍♂️": emojis.o,
+            "🙋‍♀️": emojis.p,
+            "🙋": emojis.q
+        }
+    },
+    "Custom": {
+        "messageId": process.env.SELF_ROLES_CUSTOM_ID,
+        "exclusive": false,
+        "roleIds": {
+            "📢": emojis.r,
+            "🎲": emojis.s,
+            "📣": emojis.t
+        }
+    }
+};
+
+module.exports = {
+    selfRoleReactions
+};
