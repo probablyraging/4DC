12fe4c342b5fbf1459af677858769eae2edf07a5
ProbablyRaging <probablyraging@gmail.com>
2022-01-02 13:31:34 +1100

    Prevent users spamming and creating new channels

--- events/guild/voiceStateUpdate.js
+++ events/guild/voiceStateUpdate.js
@@ -5,8 +5,10 @@ module.exports = {
     execute(oldState, newState, client, Discord) {
         const guild = client.guilds.cache.get(process.env.GUILD_ID);
         const botRole = guild.roles.cache.get(process.env.BOT_ROLE);
+        const vcHub = guild.channels.cache.get(process.env.VC_HUB);
 
         if (newState?.channelId === process.env.VC_HUB) {
+            // when a user connects to the vc_hub channel, create a unique vc channel with permissions
             newState?.guild.channels.create(`${newState?.member?.displayName}'s VC`, {
                 type: 'GUILD_VOICE',
                 permissionOverwrites: [{
@@ -14,6 +16,7 @@ module.exports = {
                     allow: ['CONNECT'],
                 }]
             }).then(vc => {
+                // give the bot the permissions it needs
                 vc.permissionOverwrites.edit(botRole, {
                     VIEW_CHANNEL: true,
                     MANAGE_CHANNELS: true,
@@ -21,11 +24,18 @@ module.exports = {
                     MOVE_MEMBERS: true,
                 }).catch(err => console.error(`${path.basename(__filename)} There was a problem editing a voice channel's permissions: `, err));
 
+                // allow the user to be able to connect (be moved to)
                 vc.permissionOverwrites.edit(newState?.member, {
                     VIEW_CHANNEL: true,
                     CONNECT: true,
                 }).catch(err => console.error(`${path.basename(__filename)} There was a problem editing a voice channel's permissions: `, err));
 
+                // disallow the user from join the hub again. this prevents creating multiple unique vc channels
+                vcHub.permissionOverwrites.edit(newState?.member, {
+                    CONNECT: false
+                }).catch(err => console.error(`${path.basename(__filename)} There was a problem editing a voice channel's permissions: `, err));
+
+                // move the user's unique channel to the vc category and we wait .5 seconds for good measure
                 let category = newState.guild.channels.cache.find(c => c.id === process.env.VC_CATEGORY && c.type === 'GUILD_CATEGORY');
 
                 voiceChannel = vc.id
@@ -33,15 +43,22 @@ module.exports = {
 
                 setTimeout(() => vc.setParent(process.env.VC_CATEGORY, { lockPermissions: false }), 500);
 
-                newState?.member?.voice.setChannel(voiceChannel).catch(err => console.error(`${path.basename(__filename)} There was a problem setting a voice channel: `, err));
+                // move the user to their new unique channel
+                newState?.member?.voice.setChannel(voiceChannel).catch(err => console.error(`${path.basename(__filename)} There was a problem moving a user to a voice channel: `, err));
             });
         }
         else if (newState?.channelId === null) {
+            // fetch and filter vc channels to see if anyone is in them, if not, we can delete them
             const fetchedChannel = guild.channels.cache.filter(c => c.type === 'GUILD_VOICE' && c.id !== process.env.VC_HUB && c.id !== process.env.VC_ONLINE && c.id !== process.env.VC_TOTAL && c.members.size < 1);
 
             fetchedChannel.forEach(channel => {
                 channel.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a voice channel: `, err));
             });
+
+            // reallow the user to join the vc_hub again
+            vcHub.permissionOverwrites.edit(newState?.member, {
+                CONNECT: true
+            }).catch(err => console.error(`${path.basename(__filename)} There was a problem editing a voice channel's permissions: `, err));
         }
     }
 }
