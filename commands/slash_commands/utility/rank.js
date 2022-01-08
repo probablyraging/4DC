240ee3073f6036a04e2ba80ec2d20bd2841934c9
HighTechLowIQ <hightechlowiq@gmail.com>
2022-01-08 14:04:08 +0000

    Slight optimisations in the rank command, and lowering filesize of rank background and medals

--- commands/slash_commands/utility/rank.js
+++ commands/slash_commands/utility/rank.js
@@ -1,8 +1,12 @@
-const { ContextMenuInteraction, MessageAttachment } = require('discord.js');
-const mongo = require('../../../mongo');
-const rankSchema = require('../../../schemas/rank-schema');
-const Canvas = require('canvas');
-const path = require('path');
+const {MessageAttachment} = require("discord.js");
+const mongo = require("../../../mongo");
+const rankSchema = require("../../../schemas/rank-schema");
+const Canvas = require("canvas");
+const path = require("path");
+
+function kFormatter(num) {
+    return Math.abs(num) > 999 ? Math.sign(num) * ((Math.abs(num) / 1000).toFixed(1)) + "K" : Math.sign(num) * Math.abs(num);
+}
 
 module.exports = {
     name: `rank`,
@@ -18,16 +22,15 @@ module.exports = {
         required: false,
     }],
     /**
-     * 
-     * @param {ContextMenuInteraction} interaction 
+     *
+     * @param {ContextMenuInteraction} interaction
      */
     async execute(interaction) {
-        const { guild, channel, member, options } = interaction;
-
-        const botChan = guild.channels.cache.get(process.env.BOT_CHAN)
+        const {guild, channel, member, options} = interaction;
+        const botChan = guild.channels.cache.get(process.env.BOT_CHAN);
 
         // make sure we only use this command in the #bot-spam channel
-        if (channel.id !== process.env.BOT_CHAN && !member.permissions.has('MANAGE_MESSAGES')) {
+        if (channel.id !== process.env.BOT_CHAN && !member.permissions.has("MANAGE_MESSAGES")) {
             return interaction.reply({
                 content: `${process.env.BOT_DENY} \`You can only use this command in\` ${botChan}`,
                 ephemeral: true
@@ -36,118 +39,116 @@ module.exports = {
 
         await interaction.deferReply();
 
-        const target = options.getMember('username') || member;
+        const target = options.getMember("username") || member;
         const targetId = target?.user?.id || member?.id;
+
+        // Load default images
+        const background = await Canvas.loadImage("./res/images/rankbg.jpg");
+        const rankFirst = await Canvas.loadImage("./res/images/firstplace.png");
+        const rankSecond = await Canvas.loadImage("./res/images/secondplace.png");
+        const rankThird = await Canvas.loadImage("./res/images/thirdplace.png");
 
         await mongo().then(async mongoose => {
             try {
-                const results = await rankSchema.find({ id: targetId }).catch(err => console.error(`${path.basename(__filename)} There was a problem finding a database entry: `, err));
+                const results = await rankSchema.find({id: targetId}).catch(err => console.error(`${path.basename(__filename)} There was a problem finding a database entry: `, err));
 
                 if (results.length === 0) {
-                    return interaction.editReply({
-                        content: `${process.env.BOT_DENY} \`You aren't ranked yet. Send some messages to earn XP\``,
-                    }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending an interaction: `, err));
+                    return interaction.editReply(`${process.env.BOT_DENY} \`The user isn't ranked yet. They need to send some messages to earn XP.\``)
+                        .catch(err => console.error(`${path.basename(__filename)} There was a problem sending an interaction: `, err));
                 }
 
                 for (const info of results) {
-                    let { username, discrim, rank, level, msgCount, xxp, xxxp } = info;
+                    let {username, discrim, rank, level, msgCount, xxp, xxxp} = info;
 
                     const rankPos = parseInt(rank);
-
-                    // create our canvas
-                    Canvas.registerFont('./res/fonts/ulm_grotesk.ttf', { family: 'grotesk' })
                     const canvas = Canvas.createCanvas(930, 280);
-                    const ctx = canvas.getContext('2d');
-
-                    // default background
-                    let background = await Canvas.loadImage('./res/images/rankbg.png');
+                    const ctx = canvas.getContext("2d");
 
                     // stretch background to the size of the canvas
                     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
                     // draw an opaque rectangle ontop of image
-                    ctx.fillStyle = 'rgba(0,0,0,0.5)';
+                    ctx.fillStyle = "rgba(0,0,0,0.5)";
                     ctx.fillRect(20, 30, canvas.width - 40, canvas.height - 60);
 
                     // try to compensate for long usernames
-                    ctx.font = '37px grotesk';
-                    let userDiscrim = username + '#' + discrim;
-                    if (userDiscrim.length > 20) ctx.font = '30px grotesk';
-                    if (userDiscrim.length > 30) ctx.font = '30px grotesk', userDiscrim = userDiscrim.slice(0, 25) + '...';
-                    ctx.fillStyle = '#ffffff';
+                    let userDiscrim = username + "#" + discrim;
+                    if (userDiscrim.length > 30) {
+                        ctx.font = "30px grotesk";
+                        userDiscrim = userDiscrim.slice(0, 25) + "...";
+                    } else if (userDiscrim.length > 20) {
+                        ctx.font = "30px grotesk";
+                    } else {
+                        ctx.font = "37px grotesk";
+                    }
+                    ctx.fillStyle = "#ffffff";
                     ctx.fillText(userDiscrim, canvas.width / 3.8, canvas.height / 2.8);
 
                     // not the same as rankPos, this is technically 'level' but we call it 'rank' as it coincides with our rank roles
-                    ctx.font = '35px grotesk';
-                    ctx.fillStyle = '#44eaff';
+                    ctx.font = "35px grotesk";
+                    ctx.fillStyle = "#44eaff";
                     ctx.fillText(`Rank ${level}`, canvas.width / 3.8, canvas.height / 1.6);
 
                     // k formatter for numbers greater than 999
-                    const xpMath = xxxp - xxp
-
-                    function kFormatter(num) {
-                        return Math.abs(num) > 999 ? Math.sign(num) * ((Math.abs(num) / 1000 * 1).toFixed(1)) + 'K' : Math.sign(num) * Math.abs(num)
-                    }
-
-                    let xp1 = kFormatter(xpMath);
                     let xp2 = kFormatter(xxxp);
-                    let xp3 = kFormatter(xxp)
+                    let xp3 = kFormatter(xxp);
                     let count = kFormatter(msgCount);
 
                     // message count
-                    ctx.font = '23px grotesk';
-                    ctx.fillStyle = '#ffffff';
-                    ctx.textAlign = 'right';
+                    ctx.font = "23px grotesk";
+                    ctx.fillStyle = "#ffffff";
+                    ctx.textAlign = "right";
                     ctx.fillText(`Message Count: ${count}`, canvas.width / 1.16, canvas.height / 1.6);
 
                     // draw and fill our xp bar
                     const percentage = Math.floor((xxp / xxxp) * 100);
                     const roundedPercent = Math.round(percentage);
 
-                    const testPerc = 100
-                    for (i = 0; i < testPerc; i++) {
-                        ctx.beginPath()
-                        ctx.lineWidth = 14
-                        ctx.strokeStyle = '#484B4E'
-                        ctx.fillStyle = '#484B4E'
-                        ctx.arc(260 + (i * 5.32), 205, 8, 0, Math.PI * 2, true)
-                        ctx.stroke()
-                        ctx.fill()
+                    const testPerc = 100;
+                    for (let i = 0; i < testPerc; i++) {
+                        ctx.beginPath();
+                        ctx.lineWidth = 14;
+                        ctx.strokeStyle = "#484B4E";
+                        ctx.fillStyle = "#484B4E";
+                        ctx.arc(260 + (i * 5.32), 205, 8, 0, Math.PI * 2, true);
+                        ctx.stroke();
+                        ctx.fill();
                     }
 
-                    var i;
-                    for (i = 0; i < roundedPercent; i++) {
-                        ctx.beginPath()
-                        ctx.lineWidth = 14
-                        ctx.strokeStyle = '#44eaff'
-                        ctx.fillStyle = '#44eaff'
-                        ctx.arc(260 + (i * 5.32), 205, 5.5, 0, Math.PI * 2, true)
-                        ctx.stroke()
-                        ctx.fill()
+                    for (let i = 0; i < roundedPercent; i++) {
+                        ctx.beginPath();
+                        ctx.lineWidth = 14;
+                        ctx.strokeStyle = "#44eaff";
+                        ctx.fillStyle = "#44eaff";
+                        ctx.arc(260 + (i * 5.32), 205, 5.5, 0, Math.PI * 2, true);
+                        ctx.stroke();
+                        ctx.fill();
                     }
 
                     // current xp and xp needed
-                    ctx.font = '24px grotesk';
-                    ctx.fillStyle = '#000000';
+                    ctx.font = "24px grotesk";
+                    ctx.fillStyle = "#000000";
                     ctx.fillText(`${xp3} / ${xp2} XP`, canvas.width / 1.525, canvas.height / 1.31);
 
-                    // position in the leadboard
-                    ctx.font = '60px grotesk';
-                    if (rankPos.length >= 3) ctx.font = '50px grotesk';
-                    if (rankPos.length >= 5) ctx.font = '45px grotesk';
+                    // position in the leaderboard
+                    if (rankPos.length >= 5) {
+                        ctx.font = "45px grotesk";
+                    } else if (rankPos.length >= 3) {
+                        ctx.font = "50px grotesk";
+                    } else {
+                        ctx.font = "60px grotesk";
+                    }
+
                     if (rankPos === 1) {
-                        const rankFirst = await Canvas.loadImage('./res/images/firstplace.png')
                         ctx.drawImage(rankFirst, 820, 50, 70, 70);
                     } else if (rankPos === 2) {
-                        const rankFirst = await Canvas.loadImage('./res/images/secondplace.png')
-                        ctx.drawImage(rankFirst, 800, 50, 90, 90);
+                        ctx.drawImage(rankSecond, 800, 50, 90, 90);
                     } else if (rankPos === 3) {
-                        const rankFirst = await Canvas.loadImage('./res/images/thirdplace.png')
-                        ctx.drawImage(rankFirst, 800, 50, 90, 90);
+                        ctx.drawImage(rankThird, 800, 50, 90, 90);
                     } else {
-                        ctx.font = '55px grotesk';
-                        ctx.fillStyle = '#44eaff';
-                        ctx.textAlign = 'right';
+                        ctx.font = "55px grotesk";
+                        ctx.fillStyle = "#44eaff";
+                        ctx.textAlign = "right";
                         ctx.fillText(`#${rankPos}`, canvas.width / 1.05, canvas.height / 2.8);
                     }
 
@@ -158,11 +159,11 @@ module.exports = {
                     ctx.clip();
 
                     // get a small res version of the user's avatar and draw it onto the canvas
-                    const avatarURL = target?.user.displayAvatarURL({ format: 'png' }) + '?size=64';
+                    const avatarURL = target?.user.displayAvatarURL({format: "png"}) + "?size=64";
                     const avatar = await Canvas.loadImage(avatarURL);
                     ctx.drawImage(avatar, 60, 60, 160, 160);
 
-                    const attachment = new MessageAttachment(canvas.toBuffer(), 'profile-image.png');
+                    const attachment = new MessageAttachment(canvas.toBuffer(), "profile-image.png");
 
                     interaction.editReply({
                         files: [attachment]
@@ -173,4 +174,4 @@ module.exports = {
             }
         }).catch(err => console.error(`${path.basename(__filename)} There was a problem connecting to the database: `, err));
     }
-}
+};
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const client = new Discord.Client({ partials: ['CHANNEL'], intents: 32767 });
+require("dotenv").config();
+const Discord = require("discord.js");
+const Canvas = require("canvas");
+const client = new Discord.Client({partials: ["CHANNEL"], intents: 32767});
 
 client.setMaxListeners(0);
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
-['command_handler', 'event_handler'].forEach(handler => {
+["command_handler", "event_handler"].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
-})
+});
+
+// Register the font we use for the /rank command
+Canvas.registerFont("./res/fonts/ulm_grotesk.ttf", {family: "grotesk"});
 
 client.login(process.env.BOT_TOKEN);
