43bef92d8c3abaf76ca2b214e7347137dd7db251
probablyraging <probablyraging@gmail.com>
2022-01-09 15:55:24 +1100

    Moved canvas.registerFont to the ready event and other minor changes

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,6 +3,7 @@ const cronjob = require('cron').CronJob;
 const moment = require('moment');
 const date = new Date();
 const path = require('path');
+const Canvas = require("canvas");
 const memberCounter = require('../../modules/member_counter');
 const statusCounter = require('../../modules/status_counter');
 const ckqCheck = require('../../modules/ckq_check');
@@ -32,28 +33,19 @@ module.exports = {
         await mongo().then(mongoose => {
             try {
                 console.log(`\x1b[36m%s\x1b[0m`, `${moment(date).format('D MMM YYYY hh:mm')}`, 'Connected to database');
-            } finally {
-                // do nothing
+            } catch (err) {
+                console.error(`${path.basename(__filename)} There was a problem connecting to the database: `, err);
             }
-        }).catch(err => console.error(`${path.basename(__filename)} There was a problem connecting to the database: `, err));
-        
-        // // Fetch messages in #self-roles to listen for messageReactionAdd/Remove - this adds them to the cache
+        });
+
+        // Register the font we use for the /rank command
+        Canvas.registerFont("./res/fonts/ulm_grotesk.ttf", { family: "grotesk" });
+
+        // Fetch messages in #self-roles to listen for messageReactionAdd/Remove - this adds them to the cache
         const reactChannel = client.channels.cache.get(process.env.SELFROLE_CHAN);
         reactChannel.messages.fetch();
 
-        // // fetch messages in #last-letter to get the most recent message's content
-        // const llChan = client.channels.cache.get(process.env.LL_CHAN);
-        // llChan.messages.fetch({ limit: 3 });
-
-        mutesCheck(message, client, Discord);
-        fetchInvites(message, client, Discord);
-        statusCounter(client);
-        memberCounter(client);
-        ckqCheck(message, client, Discord);
-        bumpCheck(message, client, Discord);
-        liveNow(message, client, Discord);
-
-        let img = 'https://www.weebly.com/editor/uploads/1/2/6/0/126006118/custom_themes/656977109613806662/files/images/CHBoostRewards.png';
+        const img = 'https://www.weebly.com/editor/uploads/1/2/6/0/126006118/custom_themes/656977109613806662/files/images/CHBoostRewards.png';
         const boostTimer = new cronjob('0 */10 * * *', function () {
             client.channels.cache.get(process.env.GENERAL_CHAN)
                 .send({
@@ -63,9 +55,16 @@ module.exports = {
                     setTimeout(() => {
                         msg.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a message: `, err))
                     }, 900000);
-                })
+                });
         });
 
         boostTimer.start();
+        mutesCheck(message, client, Discord);
+        fetchInvites(message, client, Discord);
+        statusCounter(client);
+        memberCounter(client);
+        ckqCheck(message, client, Discord);
+        bumpCheck(message, client, Discord);
+        liveNow(message, client, Discord);
     }
 };
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const Discord = require("discord.js");
-const Canvas = require("canvas");
 const client = new Discord.Client({partials: ["CHANNEL"], intents: 32767});
 
 client.setMaxListeners(0);
@@ -10,8 +9,5 @@ client.events = new Discord.Collection();
 ["command_handler", "event_handler"].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
 });
-
-// Register the font we use for the /rank command
-Canvas.registerFont("./res/fonts/ulm_grotesk.ttf", {family: "grotesk"});
 
 client.login(process.env.BOT_TOKEN);
