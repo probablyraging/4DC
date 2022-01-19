31003f459041b31a5f56c655815042eddc26d137
probablyraging <probablyraging@gmail.com>
2022-01-19 15:47:33 +1100

    When a user deletes their message in CKQ before the timer expires, we can open the channel to be reposted in - plus other minor fixes

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,20 +1,17 @@
-const path = require('path');
+const { MessageEmbed } = require('discord.js');
 const { ImgurClient } = require('imgur');
+const mongo = require('../../mongo');
+const timerSchema = require('../../schemas/timer-schema');
+const path = require('path');
+
 
 module.exports = {
     name: 'messageDelete',
     async execute(message, client, Discord) {
-        if (message?.author.id === process.env.OWNER_ID || message?.author.bot) return;
+        if (message?.author.bot) return;
 
         const guild = client.guilds.cache.get(process.env.GUILD_ID);
-        const msgDelChan = client.channels.cache.get(process.env.MSGDEL_CHAN);        
-
-        const fetchedLogs = await guild.fetchAuditLogs({
-            limit: 1,
-            type: 'MESSAGE_DELETE',
-        });
-
-        const delLog = fetchedLogs.entries.first();
+        const msgDelChan = client.channels.cache.get(process.env.MSGDEL_CHAN);
 
         let content = message?.content || ` `;
         if (message?.content.length > 1000) content = message?.content.slice(0, 1000) + '...' || ` `;
@@ -48,5 +45,47 @@ module.exports = {
         msgDelChan.send({
             embeds: [log]
         }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending an embed: `, err));
+
+        // if a user deletes there post in CKQ before the timer is up, open the channel to be reposted in
+        if (message?.channel.id === process.env.CKQ_CHAN && !message?.author.bot) {
+            const guild = client.guilds.cache.get(process.env.GUILD_ID);
+            const ckqChannel = guild.channels.cache.get(process.env.CKQ_CHAN);
+            const ckqRole = guild.roles.cache.get(process.env.CKQ_ROLE);
+
+            const searchFor = "currentTime";
+
+            const ckEmbed = new MessageEmbed()
+                .setColor("#44eaff") // GREEN
+                .setTitle(`:crown: Content King/Queen`)
+                .setDescription(`**What Is It?**
+Content King/Queen is a promo channel with a twist. Every 5 hours the channel will unlock allowing someone to post a single link to their content. The first person to post their content wins and the channel will be locked. Your content will be featured in this channel for 5 hours and you will also get the <@&878229140992589906> role. Once your 5 hours are up, your content will be deleted and the channel will be unlocked again ready for another round. To limit channel hogging the channel is on a 6 hour cool down.
+          
+**What Can I Post?**
+Links to social media, youtube channels, twitch channels, videos, highlights etc are all allowed. Please don't post anything that breaks the server rules.`);
+
+            setTimeout(() => ckqChannel.bulkDelete(10).catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a message: `, err))
+                .then(ckqChannel.send({
+                    embeds: [ckEmbed]
+                }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending an embed: `, err))), 100);
+
+            setTimeout(() => ckqRole.members.each(member => {
+                member.roles.remove(ckqRole).catch(err => console.error(`${path.basename(__filename)} There was a problem removing a role: `, err));
+            }), 200);
+
+            setTimeout(() => ckqChannel.permissionOverwrites.edit(guild.id, {
+                SEND_MESSAGES: true,
+            }).catch(err => console.error(`${path.basename(__filename)} There was a problem editing a channel's permissions: `, err)), 300);
+
+            await mongo().then(async mongoose => {
+                await timerSchema.findOneAndUpdate({
+                    searchFor
+                }, {
+                    timestamp: "null",
+                    searchFor
+                }, {
+                    upsert: true
+                }).catch(err => console.error(`${path.basename(__filename)} There was a problem updating a database entry: `, err));
+            }).catch(err => console.error(`${path.basename(__filename)} There was a problem connecting to the database: `, err));
+        }
     }
 }
