268082f9a64447f239fad3945e6863c9b131e78a
probablyraging <probablyraging@gmail.com>
2022-01-22 01:40:02 +1100

    More /index changes

--- lists/index.js
+++ lists/index.js
@@ -59,7 +59,7 @@ By participating in this server, you agree to follow [CreatorHub's Server Rules]
 > We are always accepting new applications for staff members. If you think you would make a good addition to the team, you can apply by using the \`/apply\` command and filling out the required information. To apply, you must be Rank 10 or higher. Applications are accepted and denied privately and we don't provide notification of an application being denied. If your application is accepted however, a staff member will contact you to discuss further
 
 **How can I report someone for breaking the rules?**
-> To make a report, you can send a DM including any screenshots to <@841409086960697385> and one of the staff members will review it. If the matter is more urgent, you can ping a staff member or one of the staff roles`
+> To make a report, you can use the \`/report\` command and one of the staff members will review it. Be sure to include any screenshots in your report. If the matter is more urgent, you can ping a staff member or one of the staff roles`
         ],
 
         modschoice: [
