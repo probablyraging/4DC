29aa1335f621c2ef51aa3bc99cda1a8a5b581ec3
probablyraging <probablyraging@gmail.com>
2022-01-23 03:40:22 +1100

    More changes for the nitro blacklist

--- modules/blacklist/bl_nitro.js
+++ modules/blacklist/bl_nitro.js
@@ -13,58 +13,68 @@ module.exports = (message, client) => {
     const blChan = client.channels.cache.get(process.env.BL_CHAN);
     const staffChan = client.channels.cache.get(process.env.STAFF_CHAN);
 
-    const contLow = message?.content.toLowerCase();
-
     const member = message?.member;
 
-    if (!contLow.includes('https://discord.com/') && !contLow.includes('https://discord.gg/') && contLow.includes('https://') && contLow.includes('nitro')
-        || contLow.includes('http://') && contLow.includes('nitro')
-        || contLow.includes('www.') && contLow.includes('nitro')) {
-        member?.send({
-            content: `${process.env.BOT_DENY} \`Nitro scam link detected. You have been timedout until a staff member can verify if this is a mistake or not\``
-        }).catch(() => {
-            message?.reply({
-                content: `${process.env.BOT_DENY} \`Nitro scam link detected. You have been timedout until a staff member can verify if this is a mistake or not\``,
-                allowedMentions: { repliedUser: true },
-                failIfNotExists: false
-            }).catch(err => {
-                console.error(`${path.basename(__filename)} There was a problem sending a message: `, err);
-            }).then(msg => {
-                setTimeout(() => { msg?.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a message: `, err)) }, 10000);
+    const contLow = message?.content.toLowerCase();
+
+    let ignore = false;
+    const ignoreArr = ['https://discord.com/', 'discord.com/invite', 'discord.gg/']
+    const nitroArr = ['https://', 'http://', 'www'];
+
+    for (var i in ignoreArr) {
+        if (contLow.includes(ignoreArr[i])) {
+            ignore = true;
+        }
+    }
+
+    for (var i in nitroArr) {
+        if (!ignore && contLow.includes(nitroArr[i]) && contLow.includes('nitro')) {
+            member?.send({
+                content: `${process.env.BOT_DENY} \`Nitro scam link detected. You have been timed out until a staff member can verify if this is a mistake or not\``
+            }).catch(() => {
+                message?.reply({
+                    content: `${process.env.BOT_DENY} \`Nitro scam link detected. You have been timed out until a staff member can verify if this is a mistake or not\``,
+                    allowedMentions: { repliedUser: true },
+                    failIfNotExists: false
+                }).catch(err => {
+                    console.error(`${path.basename(__filename)} There was a problem sending a message: `, err);
+                }).then(msg => {
+                    setTimeout(() => { msg?.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a message: `, err)) }, 10000);
+                });
             });
-        });
 
-        setTimeout(() => { message?.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a message: `, err)) }, 600);
+            setTimeout(() => { message?.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a message: `, err)) }, 600);
 
-        member?.timeout(86400 * 1000 * 7, 'Nitro scam link').catch(err => console.error(`${path.basename(__filename)} There was a problem adding a timeout: `, err));
+            member?.timeout(86400 * 1000 * 7, 'Nitro scam link').catch(err => console.error(`${path.basename(__filename)} There was a problem adding a timeout: `, err));
 
-        let msgContent = message?.content || ` `;
-        if (message?.content.length > 1000) msgContent = message?.content.slice(0, 1000) + '...' || ` `;
+            let msgContent = message?.content || ` `;
+            if (message?.content.length > 1000) msgContent = message?.content.slice(0, 1000) + '...' || ` `;
 
-        const blacklistEmbed = new MessageEmbed()
-            .setAuthor({ name: `${message?.author?.tag}'s message was deleted`, iconURL: message?.author?.displayAvatarURL({ dynamic: true }) })
-            .setColor('#E04F5F')
-            .addField(`Author`, `<@${message?.author?.id}>`, true)
-            .addField(`Channel`, `${message?.channel}`, true)
-            .addField(`Reason`, `Nitro scam link`, true)
-            .addField(`Message`, `\`\`\`${msgContent}\`\`\``)
-            .setFooter({ text: guild.name, iconURL: guild.iconURL({ dynamic: true }) })
-            .setTimestamp()
+            const blacklistEmbed = new MessageEmbed()
+                .setAuthor({ name: `${message?.author?.tag}'s message was deleted`, iconURL: message?.author?.displayAvatarURL({ dynamic: true }) })
+                .setColor('#E04F5F')
+                .addField(`Author`, `<@${message?.author?.id}>`, true)
+                .addField(`Channel`, `${message?.channel}`, true)
+                .addField(`Reason`, `Nitro scam link`, true)
+                .addField(`Message`, `\`\`\`${msgContent}\`\`\``)
+                .setFooter({ text: guild.name, iconURL: guild.iconURL({ dynamic: true }) })
+                .setTimestamp()
 
-        blChan.send({
-            embeds: [blacklistEmbed]
-        }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a log: `, err)).then(m => {
-            const avatarURL = client.user.avatarURL({ format: 'png', size: 256 });
+            blChan.send({
+                embeds: [blacklistEmbed]
+            }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a log: `, err)).then(m => {
+                const avatarURL = client.user.avatarURL({ format: 'png', size: 256 });
 
-            staffChan.createWebhook(client.user.username, { avatar: avatarURL }).then(webhook => {
-                webhook.send({
-                    content: `${message?.author} posted a link that looks like a Discord nitro scam. Please review [this message](${m?.url}) if it exists, and ban them if neccassary`,
-                }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a webhook message: `, err));
+                staffChan.createWebhook(client.user.username, { avatar: avatarURL }).then(webhook => {
+                    webhook.send({
+                        content: `${message?.author} posted a link that looks like a Discord nitro scam/virus. Please review [this message](${m?.url}) if it exists, and ban them if neccassary`,
+                    }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a webhook message: `, err));
 
-                setTimeout(() => {
-                    webhook.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a webhook: `, err));
-                }, 10000);
-            }).catch(err => console.error(`${path.basename(__filename)} There was a problem creating a webhook: `, err));
-        });
+                    setTimeout(() => {
+                        webhook.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a webhook: `, err));
+                    }, 10000);
+                }).catch(err => console.error(`${path.basename(__filename)} There was a problem creating a webhook: `, err));
+            });
+        }
     }
 }
