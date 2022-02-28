c9d1aa9b27495b95001fbbc18e15503b7bfdbc1c
probablyraging <probablyraging@gmail.com>
2022-03-01 05:59:05 +1100

    Updated rules list to reflect new Discord ToS

--- lists/rule-list.js
+++ lists/rule-list.js
@@ -4,11 +4,11 @@ module.exports = {
     pre: `To keep CreatorHub a safe and positive experience for everyone, you are required to follow [CreatorHub's Server Rules](<https://discord.com/channels/820889004055855144/898541066595209248>), [Discord's ToS](<https://discord.com/terms>) and [Discord's Community Guidelines](<https://discord.com/guidelines>)`,
 
     rules: [
-        "Harmful posts, usernames, nicknames, avatars, profiles or linked accounts containing viruses, pornographic, discriminatory or violent content has no place in this server. This may result in a ban without warning",
+        "Posts (messages, videos, GIFs etc..), usernames, nicknames, avatars, profiles or linked accounts containing viruses, discriminatory content, or content which is not family friendly has no place in this server. This may result in a ban without warning",
         "Do not spam or flood any channels. This means sending multiple messages rapidly or sending purposeless messages in an attempt to gain XP/Rank",
-        "Do not post self promotional content outside of the ‘SELF PROMOTE’ section. This includes sending other server members unsolicited DMs containing promotional content, server invites or spam",
+        "Do not post self promotional content outside of the 'SELF PROMOTE' section. This includes sending other server members unsolicited DMs containing promotional content, server invites or spam",
         `Advertising of other discord servers, paid services and products, or promotional content not related to content creation is only permitted in <#${process.env.PREM_CHAN}>`,
-        "We do not condone sub4sub type behaviour anywhere on the server, including the ‘SELF PROMOTE’ section. This is also against [YouTube’s ToS](<https://support.google.com/youtube/answer/3399767?hl=en>)",
+        "We do not condone sub4sub type behaviour anywhere on the server, including the 'SELF PROMOTE' section. This is also against [YouTube's ToS](<https://support.google.com/youtube/answer/3399767?hl=en>)",
         "Do not openly discuss or ask staff about other member's bans, mutes, message deletions, or other moderating actions",
         "For moderating purposes, please keep your messages in English"
     ]
