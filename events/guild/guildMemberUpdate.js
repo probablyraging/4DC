47aa5c5f02a673e57c5971397fa90d79f16b2308
ProbablyRaging <probablyraging@gmail.com>
2022-07-11 15:14:16 +1000

    Added expiration to timeout log

--- events/guild/guildMemberUpdate.js
+++ events/guild/guildMemberUpdate.js
@@ -20,12 +20,27 @@ module.exports = {
             const toReason = reason || `None`;
             const timestamp = new Date().getTime();
 
+            function converTimestampToSimpleFormat(timestamp) {
+                const t = new Date(timestamp);
+                const date = ('0' + t.getDate()).slice(-2);
+                const month = ('0' + (t.getMonth() + 1)).slice(-2);
+                const year = t.getFullYear();
+                const hours = ('0' + t.getHours()).slice(-2);
+                const minutes = ('0' + t.getMinutes()).slice(-2);
+                const seconds = ('0' + t.getSeconds()).slice(-2);
+                const time = `${date}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+                return time;
+            }
+
+            const expiresAt = converTimestampToSimpleFormat(new Date(oldMember.communicationDisabledUntilTimestamp).getTime());
+
             // Log to channel
             let log = new MessageEmbed()
                 .setColor("#E04F5F")
                 .setAuthor({ name: `${executor?.tag}`, iconURL: executor?.displayAvatarURL({ dynamic: true }) })
                 .setDescription(`**Member:** ${oldMember?.user.tag} *(${oldMember?.user.id})*
 **Action:** Timeout
+**Expires:** ${expiresAt}
 **Reason:** ${toReason}`)
                 .setFooter({ text: guild.name, iconURL: guild.iconURL({ dynamic: true }) })
                 .setTimestamp();
