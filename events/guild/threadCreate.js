3d05348cb6b55cdb1f1b422959ebc7554adceca6
Probably Raging <probablyraging@gmail.com>
2022-07-31 03:43:49 +1000

    Fixed a typo

--- events/guild/threadCreate.js
+++ events/guild/threadCreate.js
@@ -11,7 +11,7 @@ module.exports = {
             thread.send({
                 content: `This forum is focused on providing feedback about specific aspects of the creation process
 <:minidot:923683258871472248> Is your post specific about what you want feedback on?
-<:minidot:923683258871472248> Have you provide timestamps of the areas you want reviewed?
+<:minidot:923683258871472248> Have you provided timestamps of the areas you want reviewed?
 <:minidot:923683258871472248> Can you share references to what you would like to achieve?
 <:minidot:923683258871472248> Example of a good title: "How can I mix my audio better at 3:20?"
 <:minidot:923683258871472248> Example of a bad title: "Does this look good?"
