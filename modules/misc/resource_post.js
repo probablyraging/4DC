2bac7b336e068a697361b4337ad1d6ba536ab21c
Probably Raging <probablyraging@gmail.com>
2022-08-25 04:32:55 +1000

    Changed resource post

--- modules/misc/resource_post.js
+++ modules/misc/resource_post.js
@@ -5,42 +5,8 @@ const path = require('path');
  * @param {Message} message 
  */
 module.exports = async (message, client) => {
-    const resChan = client.channels.cache.get(process.env.RES_CHAN);
-
-    if (message?.channel.id === process.env.RES_CHAN && !message?.author.bot) {
-        const content = message?.cleanContent.split('\n');
-
-        let resEmbed = new EmbedBuilder()
-            .setColor('#F44336')
-            .setTitle(`${content[0]}`)
-            .setDescription(`${content.slice(1).join('\n')}`)
-            .setFooter({ text: message?.author?.tag, iconURL: message?.author?.displayAvatarURL({ dynamic: true }) })
-            .setTimestamp()
-
-        let msgAttachment = message?.attachments.size > 0 ? message?.attachments.first().url : null;
-
-        if (msgAttachment) {
-            // create a new imgur client
-            const imgur = new ImgurClient({ clientId: process.env.IMGUR_ID, clientSecret: process.env.IMGUR_SECRET });
-
-            // upload attachment to imgur, get the link and attach it to the embed
-            const response = await imgur.upload({
-                image: msgAttachment,
-            }).catch(err => console.error(`${path.basename(__filename)} There was a problem uploading an image to imgur: `, err));
-
-            if (response.length > 0) {
-                if (response[0].status !== 200) return;
-                log.setImage(resEmbed[0].data.link);
-            }
-        }
-
-        resChan.send({
-            embeds: [resEmbed]
-        }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err)).then(msg => {
-            msg.react('<a:upvote:842350442297688074>')
-            msg.react('<a:downvote:842350442481713153>')
-        }).catch(err => console.error(`${path.basename(__filename)} There was a problem adding reactions: `, err));
-
-        message.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a message: `, err));
+    if (message?.channel.id === process.env.RES_CHAN) {
+        message?.react('<a:upvote:842350442297688074>');
+        message?.react('<a:downvote:842350442481713153>');
     }
 }
