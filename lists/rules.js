9fb92398d49a37e708073bdc34efca57968833fe
Probably Raging <probablyraging@gmail.com>
2022-10-16 01:44:55 +1100

    Updated rule 5

--- lists/rules.js
+++ lists/rules.js
@@ -4,7 +4,7 @@ module.exports = {
         `This means sending multiple messages repeatedly or sending purposeless messages in an attempt to gain XP`,
         `Do not post self-promotional content outside of the 'CONTENT SHARE' section. This includes sending other server members unsolicited DMs containing promotional content, server invites, or spam`,
         `Advertising of other discord servers, offering or requesting paid services, products, and other promotional content not related to content creation is only permitted in <#${process.env.PREM_CHAN}>`,
-        `We do not allow posts that break the various platform Terms of Service. This includes content like sub for sub behaviour, promoting/linking bot votes, etc.`,
+        `We do not allow posts that break the various platform's Terms of Services. This includes content like sub4sub, buying/selling accounts, fake inflation, etc..`,
         `Do not publicly discuss other members' bans, mutes, message deletions, or other moderating actions. If you have a concern, please DM a staff member privately`,
         `For moderating purposes, please keep your messages in English`
     ]
