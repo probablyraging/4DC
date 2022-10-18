cba7f1bb8ef75c58a09190fb6024b70c51c9600b
ProbablyRaging <probablyraging@gmail.com>
2022-10-18 19:49:19 +1100

    Removes unneeded/unused code from event-handler (#18)

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -1,28 +1,14 @@
-const { events } = require('../lists/event-names');
 const { promisify } = require('util');
 const { glob } = require('glob');
 const PG = promisify(glob);
-const ascii = require('ascii-table');
 
 module.exports = async (client, Discord) => {
-    const table = new ascii('Events loaded');
-
     (await PG(`${process.cwd()}/events/*/*.js`)).map(async (file) => {
         const event = require(file);
-
-        if (!events.includes(event.name) || !event.name) {
-            const l = file.split('/');
-             table.addRow(`${event.name || 'missing'}`, `Event name invalid or missing: ${l[6] + `/` + l[7]}`);
-            return;
-        }
-
         if (event.once) {
             client.once(event.name, (...args) => event.execute(...args, client, Discord));
         } else {
             client.on(event.name, (...args) => event.execute(...args, client, Discord));
         }
-
-        table.addRow(event.name, `SUCCESSFULLY LOADED!`);
-    })
-    // console.log(table.toString()); // use to check if events loaded without error
+    });
 }
