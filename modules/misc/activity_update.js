d1b6e7fd1bd17b02604c8296484125bf2519e98c
Probably Raging <probablyraging@gmail.com>
2022-10-19 01:03:32 +1100

    Changed bot activity

--- modules/misc/activity_update.js
+++ modules/misc/activity_update.js
@@ -4,6 +4,8 @@ module.exports = (client) => {
     function numberWithCommas(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
+    const guild = client.guilds.cache.get(process.env.GUILD_ID);
+    client.user.setActivity(`${numberWithCommas(guild.memberCount)} users`, { type: ActivityType.Watching });
     setTimeout(() => {
         const guild = client.guilds.cache.get(process.env.GUILD_ID);
         client.user.setActivity(`${numberWithCommas(guild.memberCount)} users`, { type: ActivityType.Watching });
