a8b9fd1d361f00eea63ad0b874259f4137e3f6c8
Probably Raging <probablyraging@gmail.com>
2022-10-20 18:18:19 +1100

    Disable guildMemberRemove temporarily

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 module.exports = {
     name: 'guildMemberRemove',
-    async execute(member, client, Discord) {
-        const guild = client.guilds.cache.get(process.env.GUILD_ID);
-        const joinLeaveChan = client.channels.cache.get(process.env.JOINLEAVE_CHAN);
+    // async execute(member, client, Discord) {
+    //     const guild = client.guilds.cache.get(process.env.GUILD_ID);
+    //     const joinLeaveChan = client.channels.cache.get(process.env.JOINLEAVE_CHAN);
 
-        // Joins/leaves log channel
-        joinLeaveChan.send({
-            content: `${process.env.BOT_LEAVE} ${member} left. There are now **${guild.memberCount}** members in the server`,
-            allowedMentions: { parse: [] }
-        }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err));
-    }
+    //     // Joins/leaves log channel
+    //     joinLeaveChan.send({
+    //         content: `${process.env.BOT_LEAVE} ${member} left. There are now **${guild.memberCount}** members in the server`,
+    //         allowedMentions: { parse: [] }
+    //     }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err));
+    // }
 }
