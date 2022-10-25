69886ead612e834f7beecd92b968fd80cf33ae8e
Probably Raging <probablyraging@gmail.com>
2022-10-25 15:36:07 +1100

    Updated rule 4

--- lists/rules.js
+++ lists/rules.js
@@ -3,7 +3,7 @@ module.exports = {
         `Messages, usernames, avatars, banners, bios, or linked accounts containing harmful, offensive, or explicit content has no place in this server. This may result in a ban without warning`,
         `This means sending multiple messages repeatedly or sending purposeless messages in an attempt to gain XP`,
         `Do not post self-promotional content outside of the 'CONTENT SHARE' section. This includes sending other server members unsolicited DMs containing promotional content, server invites, or spam`,
-        `Advertising of other discord servers, offering or requesting paid services excluding video editors and graphics designers, paid products, and other promotional content not related to content creation is only permitted in <#${process.env.PREM_CHAN}>`,
+        `Advertising of other discord servers, offering or requesting paid services excluding video editors and graphics designers, and other paid products or promotional content is only permitted in <#${process.env.PREM_CHAN}>`,
         `We do not allow posts that break the various platform's Terms of Services. This includes content like sub4sub, buying/selling accounts, fake inflation, etc..`,
         `Do not publicly discuss other members' bans, mutes, message deletions, or other moderating actions. If you have a concern, please DM a staff member privately`,
         `For moderating purposes, please keep your messages in English`
