e038e8f218170b9043b9ce3c04661d6d906896df
Probably Raging <probablyraging@gmail.com>
2022-10-25 18:21:34 +1100

    Disclaimer for paid service threads

--- events/guild/threadCreate.js
+++ events/guild/threadCreate.js
@@ -7,6 +7,7 @@ module.exports = {
      * @param {ThreadChannel} thread
      */
     async execute(thread, newlyCreated, client) {
+        // Information for video/gfx review threads
         if (newlyCreated && thread.parentId === process.env.REVIEW_CHAN) {
             setTimeout(() => {
                 thread.send({
@@ -17,6 +18,21 @@ module.exports = {
 <:minidot:923683258871472248> Example of a good title: "How can I mix my audio better at 3:20?"
 <:minidot:923683258871472248> Example of a bad title: "Does this look good?"
 If you need to edit your title or post, please do so now or it may be deleted`
+                }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err));
+            }, 3000);
+        }
+
+        // Disclaimer for paid services in looking for stuff channel
+        if (newlyCreated && thread.parentId === process.env.LFS_CHAN && thread.appliedTags.includes('1034360626245550090')) {
+            setTimeout(() => {
+                thread.send({
+                    content: `This thread has been tagged as a Paid Service
+<:minidot:923683258871472248> Pay for services at your own risk
+<:minidot:923683258871472248> Be cautious, ask for proof of previous work, and use a reputable payment system
+ForTheContent is not responsible for any monetary loss when paying for a service in this forum. If you believe a user is scamming others, please contact a <@&1033563360660291615> member privately`,
+                    allowedMentions: {
+                        parse: []
+                    }
                 }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err));
             }, 3000);
         }
