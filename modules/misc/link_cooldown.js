47e29142751754c4e8dff25865300c22469386f4
Probably Raging <probablyraging@gmail.com>
2022-10-26 19:24:11 +1100

    Tidied up link cooldown

--- modules/misc/link_cooldown.js
+++ modules/misc/link_cooldown.js
@@ -1,43 +1,21 @@
 const { Message } = require('discord.js');
 const cooldown = new Set();
-const sleep = require("timers/promises").setTimeout;
-const path = require('path');
-/**
- * @param {Message} message 
- */
 
-module.exports = async (message, client) => {
+module.exports = async (message) => {
     const author = message?.author;
-
     const content = message?.content.toLocaleLowerCase();
     const conditions = ['https://', 'http://', 'www.'];
     const check = conditions.some(el => content.includes(el));
 
-    if (!author) {
-        return;
-    } else {
-        if (check && !message?.member?.permissions.has("ManageMessages") && !message?.member?.roles.cache.has(process.env.RANK5_ROLE) && !message?.member?.roles.cache.has(process.env.VERIFIED_ROLE) && !message?.author?.bot) {
-            if (cooldown.has(message?.author?.id)) {
-                author?.send({
-                    content: `${process.env.BOT_DENY} You're sending links too fast. Please wait 30 seconds between posting`,
-                }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a message to a user. This usually happens when the target has DMs disabled: `, err))
-                    .then(() => {
-                        setTimeout(() => {
-                            message?.delete().catch(err => console.error(`${path.basename(__filename)} 3 ### THIS IS EXPECTED SOME TIMES: `, err));
-                        }, 600);
-
-                        let msgContent = message?.content || ` `;
-                        if (message?.content.length > 1000) msgContent = message?.content.slice(0, 1000) + '...' || ` `;
-                    });
-
-                await sleep(300);
-            } else {
-                cooldown.add(message?.author.id)
-
-                setTimeout(() => {
-                    cooldown.delete(message?.author.id)
-                }, 30000);
-            }
+    if (!author) return;
+    if (check && !message?.member?.permissions.has("ManageMessages") && !message?.member?.roles.cache.has(process.env.RANK5_ROLE) && !message?.member?.roles.cache.has(process.env.VERIFIED_ROLE) && !message?.author?.bot) {
+        if (cooldown.has(message?.author?.id)) {
+            message?.delete().catch(() => { });
+        } else {
+            cooldown.add(message?.author.id)
+            setTimeout(() => {
+                cooldown.delete(message?.author.id)
+            }, 30000);
         }
     }
 }
