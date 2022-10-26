ca091bbf007e9bd0aef441bcb0fb18befba36d0a
Probably Raging <probablyraging@gmail.com>
2022-10-26 20:10:59 +1100

    Added welcome/introduction message for new members

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -7,6 +7,23 @@ module.exports = {
         const guild = client.guilds.cache.get(process.env.GUILD_ID);
         const inviteChan = client.channels.cache.get(process.env.INVITE_CHAN);
         const joinLeaveChan = client.channels.cache.get(process.env.JOINLEAVE_CHAN);
+        const generalChan = client.channels.cache.get(process.env.GENERAL_CHAN);
+
+        // Send a welcome message in the general channel
+        generalChan.createWebhook({ name: client.user.username, avatar: client.user.avatarURL({ format: 'png', size: 256 }) }).then(webhook => {
+            setTimeout(function () {
+                webhook.send({
+                    content: `Welcome to the server ${member}! Feel free to introduce yourself using our [introduction template](<https://discord.com/channels/820889004055855144/820889004055855147/1027187295398408202) when you're ready`
+                }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a webhook message: `, err)).then(wh => {
+                    setTimeout(() => {
+                        wh.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleted a webhook message: `, err))
+                    }, 300000);
+                })
+                setTimeout(() => {
+                    webhook.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a webhook: `, err));
+                }, 5000);
+            }, 25000);
+        }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a webhook: `, err));
 
         // Joins/leaves log channel
         joinLeaveChan.send({
