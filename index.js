52114298f2126a2068586b17aa3aadbd39f8856e
Probably Raging <probablyraging@gmail.com>
2022-10-26 14:52:45 +1100

    Occasianlly sweep message cache

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 console.time('Time to online');
 require("dotenv").config();
 const Discord = require("discord.js");
-const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds, Discord.GatewayIntentBits.GuildMembers, Discord.GatewayIntentBits.GuildBans, Discord.GatewayIntentBits.GuildWebhooks, Discord.GatewayIntentBits.GuildInvites, Discord.GatewayIntentBits.GuildVoiceStates, Discord.GatewayIntentBits.GuildPresences, Discord.GatewayIntentBits.GuildMessages, Discord.GatewayIntentBits.GuildMessageReactions, Discord.GatewayIntentBits.DirectMessages, Discord.GatewayIntentBits.MessageContent], partials: [Discord.Partials.Channel] });
+const client = new Discord.Client({
+    intents: [Discord.GatewayIntentBits.Guilds, Discord.GatewayIntentBits.GuildMembers, Discord.GatewayIntentBits.GuildBans, Discord.GatewayIntentBits.GuildWebhooks, Discord.GatewayIntentBits.GuildInvites, Discord.GatewayIntentBits.GuildVoiceStates, Discord.GatewayIntentBits.GuildPresences, Discord.GatewayIntentBits.GuildMessages, Discord.GatewayIntentBits.GuildMessageReactions, Discord.GatewayIntentBits.DirectMessages, Discord.GatewayIntentBits.MessageContent], partials: [Discord.Partials.Channel],
+    sweepers: {
+        messages: {
+			interval: 1200,
+			lifetime: 1100
+		}
+    }
+});
 
 require('console-stamp')(console, {
     format: ':date(yyyy-mm-dd HH:MM:ss.l Z,true) :label'
