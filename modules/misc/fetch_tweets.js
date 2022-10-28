03ef65c431d9fe477f40609cff2feedeebf0d0d7
Probably Raging <probablyraging@gmail.com>
2022-10-28 16:45:12 +1100

    Fixed twitter ids

--- modules/misc/fetch_tweets.js
+++ modules/misc/fetch_tweets.js
@@ -7,7 +7,7 @@ module.exports = async (client) => {
     const guild = client.guilds.cache.get(process.env.GUILD_ID);
     const testChan = guild.channels.cache.get(process.env.NEWS_CHAN);
 
-    const twitterUserIds = ['10228272', '309366491', '3065618342'] // YouTube, Twitch, Discord
+    const twitterUserIds = ['3031071234', '309366491', '3065618342'] // YouTube, Twitch, Discord
 
     const youtube = new cronjob('0 */2 * * *', async function () {
         let results = await tweetsSchema.find();
