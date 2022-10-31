bba27a6eecda217243c3dbb0f47b930383691db8
Probably Raging <probablyraging@gmail.com>
2022-10-31 20:24:08 +1100

    Fix for welcome message

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -20,13 +20,17 @@ module.exports = {
                     generalChan.send({
                         content: `We have a new friend! <:squee:838443107988799498> Welcome to the server <@${Array.from(newUsers).join('>, <@')}>! :wave: Feel free to introduce yourself, shout yourself out *(no links)*, or just join the chat :slight_smile:`
                     }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err)).then(message => {
-                        message.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a webhook message: `, err));
+                        setTimeout(() => {
+                            message.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a webhook message: `, err));
+                        }, 300000);
                     });
                 } else if (newUsers.size > 1) {
                     generalChan.send({
                         content: `We have some new friends! <:squee:838443107988799498> Welcome to the server <@${Array.from(newUsers).join('>, <@')}>! :wave: Feel free to introduce yourselves, shout yourselves out *(no links)*, or just join the chat :slight_smile:`
                     }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err)).then(message => {
-                        message.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a webhook message: `, err));
+                        setTimeout(() => {
+                            message.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a webhook message: `, err));
+                        }, 300000);
                     });
                 }
                 // Clear the set
