cb1da3b5f227095cb0f0f46eeab2c3e594a39120
Probably Raging <probablyraging@gmail.com>
2022-11-06 11:59:43 +1100

    Removed filter words for txt2img

--- commands/slash_commands/utility/txt2img.js
+++ commands/slash_commands/utility/txt2img.js
@@ -25,7 +25,7 @@ module.exports = {
 
         const prompt = options.getString(`prompt`);
 
-        const filter = ['naked', 'boobs', 'vagina', 'penis', 'breasts', 'nude', 'porn', 'tits', 'cock', 'dick', 'fucking', 'ass', 'cunt', 'pussy', 'piss', 'shit', 'cock', 'dick', 'sex', 'anus', 'seduce', 'kill', 'dead', 'death', 'dying', 'murder', 'slaughter'];
+        const filter = ['naked', 'boobs', 'vagina', 'penis', 'breasts', 'nude', 'porn', 'tits', 'cock', 'dick', 'fucking', 'ass', 'cunt', 'pussy', 'piss', 'shit', 'cock', 'dick', 'sex', 'anus', 'seduce', 'seductive'];
 
         for (let i in filter) {
             if (prompt.includes(filter[i])) {
