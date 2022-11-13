680e6e4f572389faeb12bcee8804d4850aaa8282
Probably Raging <probablyraging@gmail.com>
2022-11-13 12:20:20 +1100

    console-stamp in locale time

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds, Discord.GatewayIntentBits.GuildMembers, Discord.GatewayIntentBits.GuildBans, Discord.GatewayIntentBits.GuildWebhooks, Discord.GatewayIntentBits.GuildInvites, Discord.GatewayIntentBits.GuildVoiceStates, Discord.GatewayIntentBits.GuildPresences, Discord.GatewayIntentBits.GuildMessages, Discord.GatewayIntentBits.GuildMessageReactions, Discord.GatewayIntentBits.DirectMessages, Discord.GatewayIntentBits.MessageContent], partials: [Discord.Partials.Channel] });
 
 require('console-stamp')(console, {
-    format: ':date(yyyy-mm-dd HH:MM:ss.l Z,true) :label'
+    format: `${new Date().toDateString().toUpperCase()} ${new Date().toLocaleTimeString().toUpperCase()} :label`
 });
 
 client.setMaxListeners(0);
