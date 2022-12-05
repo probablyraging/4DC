4d311633e732ba4c7cdd015d88c045fee8965e83
Probably Raging <probablyraging@gmail.com>
2022-12-05 22:01:57 +1100

    Updated welcome message

--- modules/misc/welcome_check.js
+++ modules/misc/welcome_check.js
@@ -9,7 +9,7 @@ module.exports = async (client) => {
             if (newUsers.size === 0) return;
             if (newUsers.size === 1) {
                 generalChan.send({
-                    content: `We have a new friend! <:squee:838443107988799498> Welcome to the server <@${Array.from(newUsers).join('>, <@')}>! :wave: Feel free to introduce yourself, shout yourself out *(no links)*, or just join the chat :slight_smile:`
+                    content: `We have a new friend! <:squee:838443107988799498> Welcome to the server <@${Array.from(newUsers).join('>, <@')}>! :wave: Feel free to <#1049263519255777301>, or just join the chat :slight_smile:`
                 }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err)).then(message => {
                     setTimeout(() => {
                         message.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a message: `, err));
@@ -17,7 +17,7 @@ module.exports = async (client) => {
                 });
             } else if (newUsers.size > 1) {
                 generalChan.send({
-                    content: `We have some new friends! <:squee:838443107988799498> Welcome to the server <@${Array.from(newUsers).join('>, <@')}>! :wave: Feel free to introduce yourselves, shout yourselves out *(no links)*, or just join the chat :slight_smile:`
+                    content: `We have some new friends! <:squee:838443107988799498> Welcome to the server <@${Array.from(newUsers).join('>, <@')}>! :wave: Feel free to <#1049263519255777301>, or just join the chat :slight_smile:`
                 }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err)).then(message => {
                     setTimeout(() => {
                         message.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a message: `, err));
