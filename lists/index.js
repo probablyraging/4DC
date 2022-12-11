b764f65b696a8b5262d77cca732ad975007c6385
Probably Raging <probablyraging@gmail.com>
2022-12-11 11:45:48 +1100

    Updated rules

--- lists/index.js
+++ lists/index.js
@@ -20,12 +20,12 @@ Check out the [Discord Beginner's Guide](<https://dis.gd/beginnersguide>) to hel
 
         `By participating in this server, you agree to follow [ForTheContent's Server Rules](<https://discord.com/channels/820889004055855144/898541066595209248>), [Discord's ToS](<https://discord.com/terms>) and [Discord's Community Guidelines](<https://discord.com/guidelines>)
                 
-> **1.** Messages, usernames, avatars, banners, bios, or linked accounts containing harmful, offensive, or explicit content has no place in this server. This may result in a ban without warning
-> **2.** Do not send multiple messages repeatedly or send purposeless messages in an attempt to gain XP
-> **3.** Do not post self-promotional content outside of the 'CONTENT SHARE' section. This includes sending other server members unsolicited DMs containing promotional content, server invites, or spam
-> **4.** Advertising of other discord servers, offering or requesting paid services *(not including video/graphic editing)*, and other paid products or promotional content is only permitted in <#${process.env.PREM_CHAN}>
-> **5.** We do not allow posts that break the various platform's Terms of Services. This includes content like sub4sub, buying/selling accounts, fake inflation, etc..
-> **6.** Do not publicly discuss other members' bans, mutes, message deletions, or other moderating actions. If you have a concern, please DM a staff member privately
+> **1.** Messages, usernames, avatars, banners, profiles, or linked accounts containing harmful, offensive, or explicit content has no place in this server. This may result in a ban without warning
+> **2.** Do no send other server members unsolicited DMs containing promotional content, server invites, services, or spam
+> **3.** Advertising of other discord servers, offering or requesting paid services *(not including video/graphic editing)*, and other paid products or promotional content is only permitted in <#${process.env.PREM_CHAN}>
+> **4.** We do not allow posts that break the various platform's ToS. This includes content like sub4sub, buying/selling accounts, fake inflation, etc..
+> **5.** Do not post self-promotional content outside of the 'CONTENT SHARE' section
+> **6.** Do not send repeated or purposeless messages in an attempt to gain XP or tokens
 > **7.** For moderating purposes, please keep your messages in English
 
 See something that breaks the rules? Use the </report:1031245457902555213> command to notify staff
