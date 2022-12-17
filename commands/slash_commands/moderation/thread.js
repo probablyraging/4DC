2013c64ce060274b2146e2fc0f40c5b3c70c7be3
Probably Raging <probablyraging@gmail.com>
2022-12-17 23:11:12 +1100

    Added a option to thread command to delete commission threads

--- commands/slash_commands/moderation/thread.js
+++ commands/slash_commands/moderation/thread.js
@@ -13,24 +13,25 @@ module.exports = {
         type: ApplicationCommandOptionType.String,
         required: true,
         choices: [{ name: 'Solved', value: 'solved' },
-        { name: 'Close', value: 'close' }]
+        { name: 'Close', value: 'close' },
+        { name: 'Delete Commission Thread', value: 'delete' }]
     }],
     /**
      * @param {CommandInteraction} interaction 
      */
     async execute(interaction) {
-        const { options, channel } = interaction;
+        const { guild, options, channel } = interaction;
 
         await interaction.deferReply({ ephemeral: true }).catch(err => console.error(`${path.basename(__filename)} There was a problem deferring an interaction: `, err));
 
-        // Only allow this command to be ran in threads channels in the help and advice forum
+        // Only allow solved and closed to be ran in threads channels in the help and advice forum
         if (!channel.isThread()) {
             return interaction.editReply({
                 content: `${process.env.BOT_DENY} This is not a thread channel`,
                 ephemeral: true
             }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending an interaction: `, err));
         }
-        if (channel.parentId !== process.env.HELP_CHAN) {
+        if (options.getString('option') !== 'delete' && channel.parentId !== process.env.HELP_CHAN) {
             return interaction.editReply({
                 content: `${process.env.BOT_DENY} This command can only be used in a <#${process.env.HELP_CHAN}> thread`,
                 ephemeral: true
@@ -63,6 +64,24 @@ module.exports = {
                     content: `${process.env.BOT_CONF} Thread has been closed`,
                     ephemeral: true
                 }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending an interaction: `, err));
+                break;
+            }
+
+            case 'delete': {
+                const threadOwner = await guild.members.fetch(channel.ownerId);
+                if (!threadOwner) {
+                    return interaction.editReply({
+                        content: `${process.env.BOT_DENY} The owner of this thread is no longer in the server`
+                    }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending an interaction: `, err));
+                }
+                await threadOwner.send({
+                    content: `Your <#1052096719778762822> thread has been deleted as it did not follow the channel guidelines. Please make sure you read the guidelines before creating a new thread`
+                }).catch(() => {
+                    interaction.editReply({
+                        content: `${process.env.BOT_CONF} Thread deleted \nI was unable to send ${threadOwner.user.tag} a DM`
+                    }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending an interaction: `, err));
+                });
+                await channel.delete().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting a thread: `, err));
                 break;
             }
         }
