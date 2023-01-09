4c4d3bd13d01b6253c7da295fbc7d998fbd04f0a
Probably Raging <probablyraging@gmail.com>
2023-01-09 12:58:59 +1100

    Fixes markdown

--- commands/slash_commands/moderation/info.js
+++ commands/slash_commands/moderation/info.js
@@ -40,7 +40,7 @@ module.exports = {
 > ${process.env.BOT_DOC} Linking your channels/socials to your Discord profile makes it easier for other people to find your content. To link them on PC; in the bottom left of Discord, go to **user settings :gear: > connections**. To link them on iOS and Android; in the bottom right, click on **your avatar > connections > add**`],
 
             ['roles', `*Information for ${target}:*
-> ${process.env.BOT_DOC} You can assign yourself some custom roles *(also known as profile flair) in the [Channels & Roles](<https://discord.com/channels/820889004055855144/customize-community>)* channel`],
+> ${process.env.BOT_DOC} You can assign yourself some custom roles *(also known as profile flair)* in the [Channels & Roles](<https://discord.com/channels/820889004055855144/customize-community>) channel`],
 
 			['xp', `*Information for ${target}:*
 > ${process.env.BOT_DOC} By sending messages in the server, you will earn between 15 and 25 XP towards your rank. Unlocking new ranks grants access to various rewards, which can be found in the <#1005283113775157349> channel. To prevent spamming, earning XP is limited to once a minute per user. You can view your current rank by using the </rank:1040546996735451176> command in the <#837945839799500850> channel`],
