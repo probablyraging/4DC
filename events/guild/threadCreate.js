4c69dc6b9b1678fcdb54fbabe012ffc53dc69449
Probably Raging <probablyraging@gmail.com>
2023-01-11 17:30:10 +1100

    Removed notice on paid commission thread creation

--- events/guild/threadCreate.js
+++ events/guild/threadCreate.js
@@ -21,20 +21,5 @@ If you need to edit your title or post, please do so now or it may be deleted`
                 }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err));
             }, 3000);
         }
-
-        // Disclaimer for paid services in looking for stuff channel
-        if (newlyCreated && thread.parentId === process.env.COMMISSIONS_CHAN && thread.appliedTags.includes('1052097998928224286')) {
-            setTimeout(() => {
-                thread.send({
-                    content: `This thread has been tagged as a Paid Service
-<:minidot:923683258871472248> Pay for services at your own risk
-<:minidot:923683258871472248> Be cautious, ask for proof of previous work, and use a reputable payment system
-ForTheContent is not responsible for any monetary loss when paying for a service in this forum. If you believe a user is scamming others, please contact a <@&1033563360660291615> member privately`,
-                    allowedMentions: {
-                        parse: []
-                    }
-                }).catch(err => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err));
-            }, 3000);
-        }
     }
 }
