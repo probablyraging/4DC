4292914c76846e9e6c7207f86c3a5eb944fb41e8
Probably Raging <probablyraging@gmail.com>
2023-01-22 10:08:38 +1100

    Updated suggestions command

--- commands/slash_commands/administration/suggestions.js
+++ commands/slash_commands/administration/suggestions.js
@@ -13,19 +13,11 @@ module.exports = {
         description: `Choose to approve or deny the suggestion`,
         type: ApplicationCommandOptionType.String,
         required: true,
-        choices: [{ name: 'approve', value: 'approve' },
-        { name: 'deny', value: 'deny' },
-        { name: 'pigeonhole', value: 'pigeonhole' }]
+        choices: [{ name: 'approve', value: 'approve' }]
     },
     {
         name: `id`,
         description: `The message ID of the suggestions`,
-        type: ApplicationCommandOptionType.String,
-        required: true
-    },
-    {
-        name: `response`,
-        description: `The approval or denial response to add to the suggestions`,
         type: ApplicationCommandOptionType.String,
         required: true
     }],
@@ -33,63 +25,23 @@ module.exports = {
      * @param {CommandInteraction} interaction
      */
     async execute(interaction) {
-        const { guild, options, user } = interaction;
+        const { guild, options } = interaction;
         await interaction.deferReply({ ephemeral: true }).catch(err => console.error(`${path.basename(__filename)} There was a problem deferring an interaction: `, err));
 
         const id = options.getString('id');
-        const response = options.getString('response');
         const suggestionChannel = guild.channels.cache.get(process.env.SUGGESTIONS_CHAN);
         const message = await suggestionChannel.messages.fetch(id);
         const embed = message?.embeds[0];
 
         // If the message doesn't contain an embed
         if (!embed) return sendResponse(interaction, `${process.env.BOT_DENY} This message does not contain a suggestion`);
-        // If the suggestion has already been replied to
-        if (embed.footer) return sendResponse(interaction, `${process.env.BOT_DENY} This suggestion has already been responded to`);
 
         // APPROVE
         switch (options.getString('choice')) {
             case 'approve': {
-                const responseEmbed = EmbedBuilder.from(embed)
-                    .addFields({ name: `Response`, value: `\`\`\`${response}\`\`\`` })
-                    .setFooter({ text: `Approved by ${user?.tag}` })
-                    .setTimestamp()
-
-                // Edit the existing embed and add the appropriate reaction
-                await message.edit({ embeds: [responseEmbed] }).catch(err => console.error(`${path.basename(__filename)} There was a problem editing an embed: `, err));
                 await message.react(`${process.env.BOT_CONF}`).catch(err => console.error(`${path.basename(__filename)} There was a problem reacting to a message: `, err));
 
-                sendResponse(interaction, `${process.env.BOT_CONF} Response added to ${id}`);
-                break;
-            }
-
-            // DENY
-            case 'deny': {
-                const responseEmbed = EmbedBuilder.from(embed)
-                    .addFields({ name: `Response`, value: `\`\`\`${response}\`\`\`` })
-                    .setFooter({ text: `Denied by ${user?.tag}` })
-                    .setTimestamp()
-
-                // Edit the existing embed and add the appropriate reaction
-                await message.edit({ embeds: [responseEmbed] }).catch(err => console.error(`${path.basename(__filename)} There was a problem editing an embed: `, err));
-                await message.react(`${process.env.BOT_DENY}`).catch(err => console.error(`${path.basename(__filename)} There was a problem reacting to a message: `, err));
-
-                sendResponse(interaction, `${process.env.BOT_CONF} Response added to ${id}`);
-                break;
-            }
-
-            // PIGEONHOLE
-            case 'pigeonhole': {
-                const responseEmbed = EmbedBuilder.from(embed)
-                    .addFields({ name: `Response`, value: `\`\`\`${response}\`\`\`` })
-                    .setFooter({ text: `Pigeonholed by ${user?.tag}` })
-                    .setTimestamp()
-
-                // Edit the existing embed and add the appropriate reaction
-                await message.edit({ embeds: [responseEmbed] }).catch(err => console.error(`${path.basename(__filename)} There was a problem editing an embed: `, err));
-                await message.react(`${process.env.BOT_INFO}`).catch(err => console.error(`${path.basename(__filename)} There was a problem reacting to a message: `, err));
-
-                sendResponse(interaction, `${process.env.BOT_CONF} Response added to ${id}`);            
+                interaction.deleteReply().catch(err => console.error(`${path.basename(__filename)} There was a problem deleting an interaction: `, err));
                 break;
             }
         }
