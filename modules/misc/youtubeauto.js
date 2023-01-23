33f9de4ca60059093545d3b31133d29c84b204a8
Probably Raging <probablyraging@gmail.com>
2023-01-24 09:22:19 +1100

    Remove user from ytauto if they're no longer in the server

--- modules/misc/youtubeauto.js
+++ modules/misc/youtubeauto.js
@@ -1,4 +1,4 @@
-const { dbUpdateOne } = require('../../utils/utils');
+const { dbUpdateOne, dbDeleteOne } = require('../../utils/utils');
 const ytNotificationSchema = require('../../schemas/misc/yt_notification_schema');
 const tokensSchema = require('../../schemas/misc/tokens_schema');
 const res = new (require('rss-parser'))();
@@ -10,53 +10,55 @@ module.exports = async (client) => {
     const contentShare = guild.channels.cache.get(process.env.CONTENT_SHARE);
     const boostPromoChan = guild.channels.cache.get(process.env.BOOSTER_PROMO);
 
-    setInterval(async () => {
-        const results = await ytNotificationSchema.find();
+    // setInterval(async () => {
+    const results = await ytNotificationSchema.find();
 
-        for (const { channelId, videoIds, userId } of results) {
-            const member = guild.members.cache.get(userId);
-            const userTag = member.user?.tag;
-            const isStaff = member?.roles?.cache.has(process.env.STAFF_ROLE);
-            const isSubscriber = member?.roles?.cache.has(process.env.SUBSCRIBER_ROLE);
-            const isBooster = member?.roles?.cache.has(process.env.BOOSTER_ROLE);
-            const tokenResult = await tokensSchema.findOne({ userId });
-            const isTokenSub = (tokenResult?.youtubeauto - new Date()) < 1 || tokenResult?.youtubeauto !== true;
+    for (const { channelId, videoIds, userId } of results) {
+        // Check if the user is still a server member
+        const member = guild.members.cache.get(userId);
+        if (!member) return dbDeleteOne(ytNotificationSchema, { userId: userId });
 
-            if (!isBooster && !isStaff && !isTokenSub && !isSubscriber) {
-                await ytNotificationSchema.findOneAndRemove({ userId })
-                    .catch((err) => console.error(`${path.basename(__filename)} There was a problem removing a database entry: `, err));
-                staffChan.send({
-                    content: `${member} has been removed from the **YouTube Auto** list`,
-                }).catch((err) => console.error(`${path.basename(__filename)} There was a problem sending an interaction: `, err));
-            }
+        const userTag = member.user?.tag;
+        const isStaff = member?.roles?.cache.has(process.env.STAFF_ROLE);
+        const isSubscriber = member?.roles?.cache.has(process.env.SUBSCRIBER_ROLE);
+        const isBooster = member?.roles?.cache.has(process.env.BOOSTER_ROLE);
+        const tokenResult = await tokensSchema.findOne({ userId });
+        const isTokenSub = (tokenResult?.youtubeauto - new Date()) < 1 || tokenResult?.youtubeauto !== true;
 
-            res.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`, (err, resolve) => {
-                if (err) return;
-
-                resolve.items.forEach(async (item) => {
-                    const regex = item.id.replace('yt:video:', '');
-
-                    if (!videoIds.includes(regex)) {
-                        videoIds.push(regex);
-
-                        await dbUpdateOne(ytNotificationSchema, { userId }, { userId, channelId, videoIds });
-
-                        if (isBooster || isSubscriber) {
-                            contentShare.send({
-                                content: `**${userTag}** just uploaded a new video - ${item.link}`,
-                            }).catch((err) => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err));
-
-                            boostPromoChan.send({
-                                content: `**${userTag}** just uploaded a new video - ${item.link}`,
-                            }).catch((err) => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err));
-                        } else {
-                            contentShare.send({
-                                content: `**${userTag}** just uploaded a new video - ${item.link}`,
-                            }).catch((err) => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err));
-                        }
-                    }
-                });
-            });
+        if (!isBooster && !isStaff && !isTokenSub && !isSubscriber) {
+            await dbDeleteOne(ytNotificationSchema, { userId: userId });
+            staffChan.send({
+                content: `${member} has been removed from the **YouTube Auto** list`,
+            }).catch((err) => console.error(`${path.basename(__filename)} There was a problem sending an interaction: `, err));
         }
-    }, 300000);
+
+        res.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`, (err, resolve) => {
+            if (err) return;
+
+            resolve.items.forEach(async (item) => {
+                const regex = item.id.replace('yt:video:', '');
+
+                if (!videoIds.includes(regex)) {
+                    videoIds.push(regex);
+
+                    await dbUpdateOne(ytNotificationSchema, { userId }, { userId, channelId, videoIds });
+
+                    if (isBooster || isSubscriber) {
+                        contentShare.send({
+                            content: `**${userTag}** just uploaded a new video - ${item.link}`,
+                        }).catch((err) => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err));
+
+                        boostPromoChan.send({
+                            content: `**${userTag}** just uploaded a new video - ${item.link}`,
+                        }).catch((err) => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err));
+                    } else {
+                        contentShare.send({
+                            content: `**${userTag}** just uploaded a new video - ${item.link}`,
+                        }).catch((err) => console.error(`${path.basename(__filename)} There was a problem sending a message: `, err));
+                    }
+                }
+            });
+        });
+    }
+    // }, 300000);
 }
