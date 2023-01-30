4b85c10acda5669f228b39077c156bb49013fbc6
Probably Raging <probablyraging@gmail.com>
2023-01-30 12:39:20 +1100

    Minor fix

--- events/guild/guildMemberUpdate.js
+++ events/guild/guildMemberUpdate.js
@@ -18,7 +18,7 @@ module.exports = {
         }
         if (newMember._roles.includes(process.env.BOOSTER_ROLE) || newMember._roles.includes('1069409478660870235')) {
             const shareGuild = client.guilds.cache.get(process.env.SHARE_GUILD);
-            const sgMember = await shareGuild.members.fetch(newMember.id)
+            const sgMember = await shareGuild.members.fetch(newMember.id).catch(() => { });
             if (sgMember) sgMember.roles.add('1069330873637412924');
         }
 
