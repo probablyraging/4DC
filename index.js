ee5f46cfe6c75a6a1674667cdd73344d02a1fe42
Probably Raging <probablyraging@gmail.com>
2023-04-21 00:20:26 +1000

    Removed unneeded client intents

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 require("dotenv").config();
-const Discord = require("discord.js");
-const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds, Discord.GatewayIntentBits.GuildMembers, Discord.GatewayIntentBits.GuildModeration, Discord.GatewayIntentBits.GuildWebhooks, Discord.GatewayIntentBits.GuildInvites, Discord.GatewayIntentBits.GuildVoiceStates, Discord.GatewayIntentBits.GuildPresences, Discord.GatewayIntentBits.GuildMessages, Discord.GatewayIntentBits.GuildMessageReactions, Discord.GatewayIntentBits.DirectMessages, Discord.GatewayIntentBits.MessageContent, Discord.GatewayIntentBits.AutoModerationConfiguration], partials: [Discord.Partials.Channel] });
+const Discord = require('discord.js');
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildModeration,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.AutoModerationConfiguration
+    ],
+    partials: [
+        Partials.Channel
+    ],
+});
 
 console.time('Time to online');
 require('console-stamp')(console, {
@@ -8,8 +25,8 @@ require('console-stamp')(console, {
 });
 
 client.setMaxListeners(0);
-client.commands = new Discord.Collection();
-client.events = new Discord.Collection();
+client.commands = new Collection();
+client.events = new Collection();
 
 ["command_handler", "event_handler"].forEach(handler => {
     require(`./handlers/client/${handler}`)(client, Discord);
